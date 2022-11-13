7e1617dc67c2cf922706e572b8f28a930f6e5b76
Innocent60 <daegilohai@gmail.com>
2022-11-13 15:41:28 +0100

    feat:evergood

--- components/evergood.js
+++ components/evergood.js
@@ -1,5 +1,5 @@
-import { View,text} from "react-native-web"
-export default function comp(props){
+import { View,text} from "react-native"
+export default function comp({props}){
     return(
         <View>
             <text> Tobechukwu component </text>
@@ -8,42 +8,42 @@ export default function comp(props){
 
 
 }
-export function comp1(name,setName){
+export function comp1({name,setName}){
     return(
         <View>
             <text> Tobechukwu first component </text>
         </View>
     )
 }
-export function comp2(age,setAge){
+export function comp2({age,setAge}){
     return(
         <view>
             <text> Tobechukwu second component</text>
         </view>
     )
 }
-export function Comp3(status,setstatus){
+export function Comp3({status,setstatus}){
     return(
         <View>
             <text> Tobechukwu third component </text>
         </View>
     )
 }
-export function Comp4(place,setplace){
+export function Comp4({place,setplace}){
 return(
     <view>
         <text> Tobechukwu fourth component </text>
     </view>
 )
 }
-export function Comp5(school,setschool){
+export function Comp5({school,setschool}){
     return(
         <view>
             <text> Tobechukwu fifth component </text>
         </view>
     )
 }
-export function Comp6(church,setchurch){
+export function Comp6({church,setchurch}){
     return(
         <view>
             <text> Tobechukwu sixth component </text>
